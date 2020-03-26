0bcd04cf57972f4d77dd3cefcd8fbd9be136be85
twinstae <rabolution@gmail.com>
2020-03-26 19:22:13 +0900

    사이트 모델 생성
    디장고 ORM에 들어갈 모델을 만들었습니다.

--- 시각화-웹사이트/p1/proto/models.py
+++ 시각화-웹사이트/p1/proto/models.py
@@ -11,11 +11,38 @@ class Attraction(models.Model):
     def __str__(self):
         return self.name
     
-class review(models.Model):
+class Review(models.Model):
     attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
-    star =
+    star = models.PositiveSmallIntegerField() #12345로 제한
     title = models.CharField(max_length=100)
-    text = models.CharField(max_length=700)
+    text = models.CharField(max_length=1500)
     
     def __str__(self):
-        return self.name
+        return self.star + " : " + self.title
+
+
+class WordStar(models.Model):
+    attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+    star1 = models.SmallIntegerField()
+    star2 = models.SmallIntegerField()
+    star3 = models.SmallIntegerField()
+    star4 = models.SmallIntegerField()
+    star5 = models.SmallIntegerField()
+    all_star = models.IntegerField()
+    avg_star = models.DecimalField(..., max_digits=5, decimal_places=4)
+    word = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.word + " : " + self.avg_star
+
+class WordSentiment(models.Model):
+    attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+    word = models.CharField(max_length=50)
+    topic1 = models.DecimalField(..., max_digits=5, decimal_places=4)
+    topic2 = models.DecimalField(..., max_digits=5, decimal_places=4)
+    topic3 = models.DecimalField(..., max_digits=5, decimal_places=4)
+    topic4 = models.DecimalField(..., max_digits=5, decimal_places=4)
+    topic5 = models.DecimalField(..., max_digits=5, decimal_places=4)
+
+    def __str__(self):
+        return self.word
