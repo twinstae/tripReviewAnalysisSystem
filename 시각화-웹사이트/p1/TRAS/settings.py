b1d1a76b0a94eb3ff921e7450f77a1e7835eb2c1
twinstae <rabolution@gmail.com>
2020-04-02 13:34:51 +0900

    사이트 프로토

--- 시각화-웹사이트/p1/TRAS/settings.py
+++ 시각화-웹사이트/p1/TRAS/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'proto.apps.ProtoConfig',
+    'proto',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TRAS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- 시각화-웹사이트/p1/TRAS/urls.py
+++ 시각화-웹사이트/p1/TRAS/urls.py
@@ -1,15 +1,12 @@
-"""
-    path('TRAS/', views.home, name='home'),
-    path('TRAS/attraction/', views.attraction_index, name='attraction_index'),
-    path('TRAS/attraction/<int:attraction_id>', views.attraction, name='attraction'),
-    path('TRAS/sort/', views.sort_index, name='sort_index'),
-    path('TRAS/sort/<int:big_Sort_id>', views.sorting, name='sorting'),
-"""
-
 from django.contrib import admin
 from django.urls import path
 from proto import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('proto/', views.HomeView.as_view(), name='home'),
+    path('proto/attraction/', views.AttractionIndexView.as_view(), name='attraction_index'),
+    path('proto/attraction/<int:attraction_id>', views.AttractionDetailView.as_view(), name='attraction_detail'),
+    path('proto/sort/', views.SortIndexView.as_view(), name='sort_index'),
+    path('proto/sort/<int:big_Sort_id>', views.SortDetailView.as_view(), name='sort_detail'),
 ]
