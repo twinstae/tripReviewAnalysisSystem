31dcd9c3bfd851885b6852e766d071b3b428e1ad
twinstae <rabolution@gmail.com>
2020-06-08 19:09:18 +0900

    star bar

--- 시각화-웹사이트/p1/static/js/Card.js
+++ 시각화-웹사이트/p1/static/js/Card.js
@@ -26,13 +26,26 @@ function Card(props) {
   return (
     <div className="w3-card w3-round-large w3-animate-left">
 		<img src={props.attraction.img_src} alt="title" style={{width:"100%"}} className="w3-round-large"/>
-		<h3>{props.attraction.name}</h3>
-		<span className="w3-round-xxlarge w3-tag" style = {{
-		 backgroundColor:'#fff0b3',
-		color:'#ff9900'
-		}}> 3.5</span>
-		<span className="w3-round-xxlarge w3-tag w3-color"> tag_name </span>		
-		<button onClick={selected} className="w3-margin w3-btn w3-medium w3-round-large w3-blue"> SELECT </button>
+		<h4>{props.attraction.name}</h4>
+		<div>
+		  <div className="w3-col" style={{width:'3%',
+		  backgroundColor:'#ff6666',
+		  color:"#ff0000"}}>.</div>
+		  <div className="w3-col" style={{width:'5%',
+		  backgroundColor:'#ff9966',
+		  color:'#ffcc66'}}>.</div>
+		  <div className="w3-col" style={{width:'20%',
+		  backgroundColor:'#fff0b3',
+		  color:'#ffdb4d'}}>★</div>
+		  <div className="w3-col" style={{width:'32%',
+		  backgroundColor:'#ffcc66',
+		  color:'#fff0b3'}}>★</div>
+		  <div className="w3-col" style={{width:'40%',
+		  backgroundColor:'#ffdb4d',
+		  color:'#ff9900'}}>avg : 4.5</div>
+		</div>
+		<span className="w3-round-xxlarge w3-tag w3-blue"> tag_name </span>	
+		<button onClick={selected} className="w3-margin w3-tag w3-round-large w3-blue"> SELECT </button>
     </div>
     );
 }
