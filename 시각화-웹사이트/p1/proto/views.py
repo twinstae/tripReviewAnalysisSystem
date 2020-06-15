ab19b8b65c065d83620afd0c74070a067fcdd4ad
twinstae <rabolution@gmail.com>
2020-06-15 19:41:36 +0900

    텍스트 유사도

--- 시각화-웹사이트/p1/proto/views.py
+++ 시각화-웹사이트/p1/proto/views.py
@@ -7,10 +7,24 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.serializers import serialize
 
+import gensim
+from gensim.utils import SaveLoad as SL
+
 import numpy as np
 import pandas as pd
 from decimal import *
+
+model = SL.load("doc2model")
+df = pd.read_csv('testCorpus.csv')
+test_corpus = []
+for a_list in df.values.tolist():
+    if np.nan in a_list:
+        test_corpus.append(a_list[:a_list.index(np.nan)])
+    else:
+        test_corpus.append(a_list)
+
 getcontext().prec = 4
+
 class LazyEncoder(DjangoJSONEncoder):
     def default(self, obj):
         if isinstance(obj, Attraction):
@@ -45,7 +59,7 @@ class MapView(generic.ListView):
 
     def get_queryset(self):  # 컨텍스트 오버라이딩
         #별점이 최상위인 여행지 5개를 넣고 싶다!
-        attractions_list = Attraction.objects.all().order_by('-star_rating')[:6]
+        attractions_list = Attraction.objects.all().order_by('-star_rating')[:5]
 
         assert len(attractions_list) > 1
         return attractions_list
@@ -71,12 +85,13 @@ def recommend(selected_attractions, pk):
     candidate_df = distance_r(selected_attractions, pk, n = 50)
     candidate_df = candidate_df.sort_values(by='end_pk')
     candidate_df['star'] = star_r(candidate_df['end_pk'])
-    candidate_df['rating'] = candidate_df['dist'] * candidate_df['star']
+    #candidate_df['doc2'] = doc2_r(pk, candidate_df['end_pk'])
+    candidate_df['rating'] = candidate_df['dist'] * candidate_df['star'] #* candidate_df['doc2']
 
     top_5 = candidate_df.sort_values(by='rating')[-5:]
 
     print(top_5)
-    print(top_5.end_pk)
+
     print(list(top_5.end_pk))
 
     new_pk_list = list(top_5.end_pk) #상위 5개의 end_pk값을 가져온다.
@@ -85,7 +100,7 @@ def recommend(selected_attractions, pk):
 
 def distance_r(selected_attractions, pk, n = 15):
 
-    candidate_df = pd.DataFrame(np.zeros(shape = (n,3)), columns = ['rating', 'end_pk', 'dist'])
+    candidate_df = pd.DataFrame(np.zeros(shape = (n,4)), columns = ['rating', 'end_pk', 'dist', 'doc2'])
     start = Attraction.objects.get(pk = pk)
     now_i = 0
 
@@ -104,15 +119,26 @@ def star_r(end_pk_series):
 
     end_qs = Attraction.objects.filter(pk__in = end_pk_series)
     star_list = end_qs.values_list('star_rating', flat = True)
-    print(end_pk_series)
-    print(len(end_pk_series))
-    print(len(star_list))
 
     assert len(end_pk_series) == len(star_list)
 
     return star_list
     #return Attraction.objects.filter(pk__in = list(end_pk_series)).values_list('star_rating', flat=True)
 
+sims_dict = {i : Decimal(1.00) for i in range(180)}
+
+def doc2_r(pk, end_pk_series):
+
+    inferred_vector = model.infer_vector(test_corpus[pk])
+    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+
+    for i, value in sims:
+        sims_dict[i] = sims_dict[i] * (Decimal(value) + 15) /Decimal(15.00)
+
+    result = [sims_dict[end_pk] for end_pk in end_pk_series]
+    assert len(end_pk_series) == len(result)
+
+    return result
 
 """def new_r(request):
     pk = request.POST.get('pk', None)
